822e6878b0851d95ac479928ae0fa30eee2158d9
Vijaysinh Shewale <91977626+vijushewale@users.noreply.github.com>
2023-05-05 18:15:08 +0530

    Add files via upload

---
+++ Employee+Management+app-+coding+files/coding files/emp_serv/src/main/java/com/emp_serv/emp_serv/model/Employee.java
@@ -0,0 +1,52 @@
+package com.emp_serv.emp_serv.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name="emp")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    //Checking columns of database
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="email")
+    private String email;
+
+    //Getter & setter method
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
